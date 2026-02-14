711284678a74d765e876784cd482aa1ffb592688
Nitinsingh7643 <nitinsingh6207816283@gmail.com>
2026-02-14 18:46:27 +0530

    debug: Log environment variables to troubleshoot deployment

--- server/src/server.ts
+++ server/src/server.ts
@@ -12,7 +12,15 @@ import couponRoutes from './routes/couponRoutes';
 import aiRoutes from './routes/aiRoutes';
 
 // Load env vars
+// Load env vars
 dotenv.config();
+
+console.log('--- Environment Check ---');
+console.log('NODE_ENV:', process.env.NODE_ENV);
+console.log('PORT:', process.env.PORT);
+console.log('MONGO_URI exists:', !!process.env.MONGO_URI); // Log if present (don't log value for security)
+console.log('MONGO_URI length:', process.env.MONGO_URI ? process.env.MONGO_URI.length : 0);
+console.log('-------------------------');
 
 // Connect to database
 connectDB();
